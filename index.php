6c2aa6adebaa2f50662be43c8471e1fd3ac55c64
Diego <diegommorais12@gmail.com>
2020-07-15 18:08:03 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include_once "template/header.php";
+?>
+
+
+<!--==========================
+    Intro Section
+  ============================-->
+<?php include_once "template/intro.php" ?>
+<?php include_once "template/service.php" ?>
+
+<!--==========================
+    Footer
+  ============================-->
+<?php include_once "template/footer.php"?>
